d00c802a4afab91f4555f954229f19f71eeea4a8
Hiwjha400 <024aaz17@gmail.com>
2026-02-28 22:13:29 +0300

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -9,10 +9,10 @@ const PARAMETER_GAME_LENGTH = 'GameLength';
 export function game_mode_length_seconds() {
     const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
     switch (length) {
-        case 'Length_S': return 210; // 3:30
-        case 'Length_M': return 270; // 4:30
-        case 'Length_L': return 330; // 5:30
-        case 'Length_XL': return 390; // 6:30
+        case 'Length_S': return 210; // 3000:30
+        case 'Length_M': return 270; // 4000:30
+        case 'Length_L': return 330; // 5000:30
+        case 'Length_XL': return 390; // 6000:30
     }
     return 270;
 }
